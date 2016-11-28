a7299dc3049a2336fea10b7f20391344a095035c
zmielna <zmielna@users.noreply.github.com>
2016-11-28 14:02:16 +0000

    Cosmetic changes

--- mysql_dump.sh
+++ mysql_dump.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#############################################################################
+############################################################################################
 # This is a simple script to create a snapshot of a MySQL server.
 # It finds all databases in the server, and creates a single gzipped dump
 # of each one using the mysqldump command, suitable for being backed up by bacula.
@@ -7,7 +7,7 @@
 # Run it from cron like that
 # 01 4 * * * /root/bin/dumpmysql.sh 2>&1 |logger
 # Send bugreports, fixes, enhancements, t-shirts, money, beer & pizza to devnull@mielnet.pl
-#############################################################################
+#############################################################################################
 
 #------------ variables
 # Directory to store backups in
